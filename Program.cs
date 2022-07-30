64c7518dd657877448edcabc89954dbbc17792b7
Hugo Victor <hugovic.it@gmail.com>
2022-07-30 15:27:44 -0300

    estrutura basica e primeiro mapget

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<AppDbContext>(opt=> opt.UseInMemoryDatabase("TrefasDb"));
-
 
 var app = builder.Build();
 
@@ -19,35 +14,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.MapGet("/",()=> "Ola mundo");  
 
-
-app.MapGet("/",() => "Olá Mundo!");
-app.MapGet("frases",async() =>
-    await new HttpClient().GetStreamAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+app.MapGet("frases",async () =>
+    await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
 );
 
 
-
-
-
 app.Run();
-
- 
-
- class Tarefa {
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsConcluida { get; set; }
-    
- }
-
- class AppDbContext: DbContext{
-
-    public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
-    {}
-
-    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
- }
-
  
